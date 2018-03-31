40bd326c3eed78db3649aac95fddb5cf16741e80
hikariyoru <35426142+hikariyoru@users.noreply.github.com>
2018-03-31 22:43:09 +0700

    Add files via upload
    based on jamesadhitthana's .csv file

---
+++ planes.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(leaflet)
+
+planeData <- read.csv(url("https://raw.githubusercontent.com/jamesadhitthana/UPH_OR_Planes/master/JamesAircraftList%20March31%20CONVERTED.csv"))
+#Using jamesadhitthana's csv file
+
+ui <- fluidPage(
+  titlePanel("2000 Airplane Flights Mapping"),
+  leafletOutput("flightmap")
+)
+
+server <- function(input, output, session) {
+  output$flightmap <- renderLeaflet({
+    leaflet(data = planeData[1:2000,]) %>% addTiles() %>% #Only 2000 because anymore than this makes my PC lag	
+      addMarkers(~JamesLong, ~JamesLat, popup = ~as.character(JamesPlaneModel), label=~as.character(JamesPlaneAirlines))
+    
+  })
+}
+
+shinyApp(ui, server)
